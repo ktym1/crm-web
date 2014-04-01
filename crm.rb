83ff8092b62986e4e4816bfb394858a70be1a2ad
Kerry Mui <kerry.t.mui@gmail.com>
2014-04-01 16:59:03 -0400

    added contacts.erb link

--- crm.rb
+++ crm.rb
@@ -1,7 +1,10 @@
 require "sinatra"
 
 get '/' do 
-	@crm_app_name = "My CRM"
+	@crm_app_name = "My CRM" #setting up an instance variable that we can pass along
 	erb :index		#"Main Menu" - we're replacing 'Main menu' with index.erb
 end
 
+get '/contacts' do
+	erb :contacts
+end
