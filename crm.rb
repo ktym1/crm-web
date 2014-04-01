652e66ce3939cd6b7389699be5ba4199d619573b
Kerry Mui <kerry.t.mui@gmail.com>
2014-04-01 19:16:46 -0400

    updated input form for new contact, updated @contacts array with @@rolodex

--- crm.rb
+++ crm.rb
@@ -2,13 +2,15 @@ require_relative "contact"
 require_relative "rolodex"
 require "sinatra"
 
+@@rolodex= Rolodex.new #create a class variable before the 'routes' (route = get "/") so Sinatra can access it from anywhere in route blocks and views
+
 get '/' do 
 	@crm_app_name = "My CRM" #setting up an instance variable that we can pass along
 	erb :index		#"Main Menu" - we're replacing 'Main menu' with index.erb
 end
 
 get '/contacts' do #view all contact
-	@contacts = [] #creating an empty array to store my instances of Contact class
+	#@contacts = [] # no longer need because the @@rolodex class instance gives ua access to contacts array #create an empty array to store Contact class instances; 
 	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
 	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
 	@contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
