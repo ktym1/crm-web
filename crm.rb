e3407ab49f36dcc36a420fcbf50b2eb317b29c2d
Kerry Mui <kerry.t.mui@gmail.com>
2014-04-02 19:52:38 -0400

    delete function added

--- crm.rb
+++ crm.rb
@@ -1,39 +1,47 @@
+require "sinatra"
+
 require_relative "contact"
 require_relative "rolodex"
-require "sinatra"
+
 
 @@rolodex= Rolodex.new #create a class variable before the 'routes' (route = get "/") so Sinatra can access it from anywhere in route blocks and views
 
 get '/' do 
-	@crm_app_name = "My CRM" #setting up an instance variable that we can pass along
+	@crm_app_name = "Kerry's CRM" #setting up an instance variable that we can pass along
 	erb :index		#"Main Menu" - we're replacing 'Main menu' with index.erb
 end
 
 # ***no longer need @contacts array, because @@rolodex class instance can access contacts array 
-get '/contacts' do #view all contact
+#get '/contacts' do #view all contact
 #    #@contacts = [] #create an empty array to store Contact class instances; 
  	# @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
  	# @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
  	# @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 
- 	erb :contact_list
- end
-
 get '/contacts/new' do
 	erb :contact_new
 end
 
-get '/contacts/:id' do #view a contact
-	erb :contact_id
+post '/contacts' do
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:role]) #initializing a class of Contact to pass through the Rolodex method
+	@@rolodex.add_a_contact(new_contact) #this arguement doesn't need to be foo. it's only the object that's being created and passed to rolodex
+ 	redirect to('/contacts')
 end
+
+get '/contacts/:id/delete' do
+	@@rolodex.delete_a_contact(params[:id])
+	redirect to('/contacts')
+end
+
+
+get '/contacts' do 
+ 	erb :contact_list
+end
+
 
 get '/contacts/:id/edit' do #modify an existing contact
 	erb :contact_edit
 end
 
-post '/contacts' do #using params to submit a request/form; contains body of the request; params hash is avialable in any route block
-	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:role]) #initializing a class of Contact to pass through the Rolodex method
-	@@rolodex.add_a_contact(new_contact)
-	redirect to('/contacts')
-end
+
 
--- rolodex.rb
+++ rolodex.rb
@@ -7,9 +7,21 @@ class Rolodex
 	end
 
 
-	def add_a_contact(contact)
-		contact.id = @id
-		@contacts << contact
+	def add_a_contact(foo)
+		# foo is actually going to be a Contact object (instance of Contact)
+		foo.id = @id #passing in the argument from crm.rb, so need to refer as new_contact in method
+		@contacts << foo
 		@id += 1
+	end
+
+	def delete_a_contact(foo)
+		# in this case, foo is actually a string representing an ID
+		result = nil
+		@contacts.each do |contact| 
+			if contact.id == foo.to_i
+				result = contact
+			end
+		end 
+		@contacts.delete(result) if result != nil
 	end
 end
